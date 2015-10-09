f616a1d5744d47a06bfb5d5d294443d7e28f2bea
Olubukonla Ayankoya <bukky.yank@gmail.com>
2015-10-09 11:50:04 +0100

    A new method added

--- first_maven_project/src/main/java/com/live/magento/pages/BukkyPage.java
+++ first_maven_project/src/main/java/com/live/magento/pages/BukkyPage.java
@@ -2,10 +2,15 @@ package com.live.magento.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class BukkyPage {
+ public class BukkyPage {
+   WebDriver driver = new FirefoxDriver();
  
-public void BukkyPage(WebDriver driver){
-	
+ public void BukkyPage(WebDriver driver){
+   this.driver = driver;	
+}
+ public void LogIn(){
+	driver.findElements(By.id("password"));
 }
 }
